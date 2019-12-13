c84af8e4dc197479dc944b9c5a8374ce4dde4f75
tfd-godream <58512508+tfd-godream@users.noreply.github.com>
2019-12-13 13:52:56 +0100

    Added test

---
+++ ConsoleApp1/ConsoleApp1/Business.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleApp1
+{
+    public static class Business
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ ConsoleApp1/TestProject1/UnitTest1.cs
@@ -0,0 +1,19 @@
+using ConsoleApp1;
+using NUnit.Framework;
+
+namespace TestProject1
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(3,Business.Add(1, 2));
+        }
+    }
+}
